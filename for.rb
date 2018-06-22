6af886c3deca3aeafe6928cbc862ee3031d7c5b7
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-22 11:50:31 +0000

    automatically backed up by learn

--- for.rb
+++ for.rb
@@ -3,7 +3,9 @@ def using_for
 	checklist = 1..10
 	#your code here
 	for item in checklist
-	  10.times puts "Wingardium Leviosa"
+	  10.times do 
+	    puts "Wingardium Leviosa"
+	  end
 	end
 end
 
